7cff0a658e390321a9c9b26ff8915c4ce351ca6c
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2022-07-22 19:49:38 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ QUAL="normal" # FFMPEG quality preset
 YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2" # Youtube RTMP base URL
 IMAGE="https://i.imgur.com/R5wA66L.jpg" #Picture
 SOURCE="http://145.239.254.146:8060/192h" # Radio Station
-KEY="rtb2-3kde-30af-jyre-463g" # Stream name/key
+KEY="z86v-sa8y-qk1k-w8tv-7r3e" # Stream name/key
 SIZE="1280x720"
 FRAMERATE="2"
 
@@ -22,14 +22,14 @@ FRAMERATE="2"
     	-vcodec libx264 \
     	-pix_fmt yuv420p \
     	-maxrate 1800k \
-    	-bufsize 5000k \
+    	-bufsize 2300k \
         -framerate 30 \
     	-g 2 \
     	-strict experimental \
     	-f flv \
     	"$YOUTUBE_URL/$KEY"
      ffplay -fflags nobuffer -flags low_delay -framedrop \
-        -strict experimental -rtsp_transport tcp rtsp://2a01:4f8:13a:19e5:1041:fc65:e2c7:0001:22
+        -strict experimental -rtsp_transport tcp rtsp://2a01:4f8:211:1e1a:face:0d35:526e:0001:22
      
 #
 #  Created by Roman on 04/22/2019.
