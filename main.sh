54eddf58857c3e81857942d9693b00dfd60c992d
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2022-07-24 19:28:37 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -26,7 +26,7 @@ FRAMERATE="2"
     	-bufsize 2500k \
         -framerate 30 \
         -r "$FPS" \
-    	-g 4 \
+    	-g 1 \
         -bf 2 \
         -movflags faststart \
     	-strict experimental \
