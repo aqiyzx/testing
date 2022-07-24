c133e457d9928d13d01b591b10ded57ac72a1c73
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2022-07-24 18:03:04 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-BITRATE="1800k" # Bitrate of the output video
+BITRATE="2500k" # Bitrate of the output video
 FPS="30" # FPS video output
 QUAL="normal" # FFMPEG quality preset
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2" # Youtube RTMP base URL
